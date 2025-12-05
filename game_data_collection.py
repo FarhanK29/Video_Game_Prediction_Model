3fbba3316fe56e0f111390afff423f166d65a092
fwakks <akshaymaburi@gmail.com>
2025-12-05 16:17:42 -0500

    updated collection to include estimations for launch data

--- game_data_collection.py
+++ game_data_collection.py
@@ -5,10 +5,8 @@ import numpy as np
 
 
 
-
-
 BASE_URL = "https://api.gamalytic.com/game/{}"
-GAME_LIST_CSV = "data/games_list.csv"
+GAME_LIST_CSV = "data/games_data_list.csv"
 OUTPUT_CSV = "data/games_data_list.csv"
 
 def get_gamalytic_info(appid:str):
@@ -29,16 +27,20 @@ def add_game_data():
     for i in range(len(df)):
         # cleans game name to remove the trademark symbols
         df.loc[i,'name'] = df.iloc[i]['name'].encode("ascii", errors="ignore").decode()
+        print(f"{i+1}/{len(df)}: {df.iloc[i]['name'].encode("ascii", errors="ignore").decode()}")
 
         appid = df.iloc[i]['appid']
 
         data = get_gamalytic_info(appid)
 
         df.loc[i,'total_reviews'] = data.get('reviewsSteam') or np.nan
+        df.loc[i,'estimated_launch_reviews'] = data.get('reviewsSteam',0) * 0.1
         df.loc[i,'followers'] =  data.get('followers') or np.nan
+        df.loc[i,'estimated_launch_followers'] = data.get('followers',0) * 0.1
         df.loc[i,'review_score'] = data.get('reviewScore') or np.nan
         df.loc[i,'avg_playtime'] = data.get('avgPlaytime') or np.nan
         df.loc[i,'copies_sold'] = data.get('copiesSold') or np.nan
+        df.loc[i,'estimated_launch_copies_sold'] = data.get('copiesSold',0) * 0.1
         df.loc[i,'revenue'] = data.get('revenue') or np.nan
         df.loc[i,'players'] = data.get('players') or np.nan
         df.loc[i,'owners'] = data.get('owners') or np.nan
