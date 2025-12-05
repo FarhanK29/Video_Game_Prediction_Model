9fdc30b1a87a2e32c3edf471a217f8b08b093746
fwakks <akshaymaburi@gmail.com>
2025-12-05 16:42:00 -0500

    updated to prevent rate limiting

--- clipboard_game_link.py
+++ clipboard_game_link.py
@@ -19,10 +19,11 @@ BASE_URL = "https://steamdb.info/app/{}/charts/#max"
 
 
 # iterate through to update these concurrent players as we were rate limited 
-data = [220, 320, 360, 550, 7670, 8850, 9480, 729040, 1030840, 1086940, 1088710, 1091500, 1105500, 1105510, 1113000, 1172470, 1174180, 1235140, 1237970, 1238810, 1245620, 1388590, 2096600, 2096610, 2208920, 2221920, 2231380, 2239550, 2369390]
+data = [8850, 9480, 729040, 1030840, 1086940, 1088710, 1091500, 1105500, 1105510, 1113000, 1172470, 1174180, 1235140, 1237970, 1238810, 1245620, 1388590, 2096600, 2096610, 2208920, 2221920, 2231380, 2239550, 2369390]
 i = 0
 for appid in data:
     print (f'{i}/{len(data)}')
+    i+=1
     url = BASE_URL.format(appid)
     webbrowser.open(url)
     time.sleep(15) # to avoid rate limiting
